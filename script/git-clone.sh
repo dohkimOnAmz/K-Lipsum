5cd6cfb0f66c4e6b536dd6b2071ba9fb841087f5
dohkim <dohkim@amazon.com>
2022-10-20 18:18:13 +0900

    :hammer:

--- script/git-clone.sh
+++ script/git-clone.sh
@@ -3,6 +3,10 @@
 git clone https://github.com/dohkimOnAmz/K-Lipsum.git
 cd K-Lipsum/
 
+# config Credential
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
+
 #Push new repo(Code Commit)
 #eg. git remote add new https://git-codecommit.us-west-2.amazonaws.com/v1/repos/commit-builders-klipsum
 git remote add new [[Commit-URL]]
